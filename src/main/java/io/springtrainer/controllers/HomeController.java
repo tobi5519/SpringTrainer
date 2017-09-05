cc82cc4c5ac8a71f7609802fd2bd04ee6de001f6
T.K. Thomsen <tobyrius@gmail.com>
2017-09-05 20:50:19 +0200

    Update

--- src/main/java/io/springtrainer/controllers/HomeController.java
+++ src/main/java/io/springtrainer/controllers/HomeController.java
@@ -3,6 +3,8 @@ package io.springtrainer.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -24,5 +26,17 @@ public class HomeController {
         model.addAttribute("name", name);
         return "about";
     }
+    /* Catch all mapping
+    @RequestMapping(value="/**", method = RequestMethod.GET)
+    public String handleResults() {
+        return "test";
+    }
+    */
+
+    //@GetMapping("/error")
+    //public String error(){
+    //    return "error";
+    //}
+
 
 }
