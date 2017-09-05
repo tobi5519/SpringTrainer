c5a18610bbd7c2f17a75f3be0b7cec080a41d5e2
T.K. Thomsen <tobyrius@gmail.com>
2017-09-05 18:51:42 +0200

    Update

--- src/main/java/io/springtrainer/controllers/HomeController.java
+++ src/main/java/io/springtrainer/controllers/HomeController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    int num = 0;
+
     @GetMapping("/")
     public String index(Model model){
+        num++;
+
+        model.addAttribute("num", num);
         return "index";
     }
 
